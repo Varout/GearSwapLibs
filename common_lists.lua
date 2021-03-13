aad3ad954f5b046fd4482d1736c1e084fd7aa5b6
Jason Auger <jason.auger@gmail.com>
2021-03-14 04:56:29 +1300

    Updated formatting and added new zones for movement wear

--- common_lists.lua
+++ common_lists.lua
@@ -2,8 +2,7 @@
 -- Zones For Special Equipment Effects
 -------------------------------------------------------------------------------------------------------------------
 --  Used for ToAU ring in assault or salvage areas
-zones_toau_ring = 
-S{
+zones_toau_ring = S{
     "Leujaoam Sanctum",             --  Assault
     "Mamool Ja Training Grounds",   --  Assault
     "Lebros Cavern",                --  Assault
@@ -13,20 +12,43 @@ S{
     "Bhaflau Remnants",             --  Salvage
     "Arrapago Remnants",            --  Salvage
     "Silver Sea Remnants",          --  Salvage
-    "Zhayolm Remnants"              --  Salvage
+    "Zhayolm Remnants",             --  Salvage
 }
 
 --  Used for SoA body movement speed
-zones_adoulin_body = 
-S{
+zones_adoulin_body = S{
     "Western Adoulin",
-    "Eastern Adoulin"
+    "Eastern Adoulin",
+    "Mog Garden",
 }
+
+zones_sandy_body = S{
+    "Southern San d'Oria",
+    "Northern San d'Oria",
+    "Port San d'Oria",
+    "Chateau d'Oraguille",
+}
+
+zones_bastok_body = S{
+    "Bastok Mines",
+    "Bastok Markets",
+    "Port Bastok",
+    "Metalworks",
+}
+
+zones_windy_body = S{
+    "Windurst Walls",
+    "Windurst Waters",
+    "Windurst Woods",
+    "Port Windurst",
+    "Heavens Tower",
+}
+
+
 -------------------------------------------------------------------------------------------------------------------
 -- Spell Sets
 -------------------------------------------------------------------------------------------------------------------
-spells_resistance =
-S{
+spells_resistance = S{
     "Barstone",     "Barstonra",
     "Barwater",     "Barwatera",
     "Barareo",      "Baraera",
@@ -40,57 +62,52 @@ S{
     "Barsilence",   "Barsilencera", --  Base: Aero
     "Barpetrify",   "Barpetra",     --  Base: Stone
     "Barvirus",     "Barvira",      --  Base: Fire
-    "Baramnesra"                    --  Base: Fire
+    "Baramnesra",                   --  Base: Fire
 }
 
-spells_na = 
-S{
+spells_na = S{
     "Poisona",
     "Paralyna",
     "Blindna",
     "Silena",
     "Stona",
-    "Viruna"
+    "Viruna",
 }
 
-spells_cursna = 
-S{
+spells_cursna = S{
     "Cursna",
 }
 
-enfeebling_int = 
-S{
-    "Blind",-- "Blind II",
-    "Poison",-- "Poison II",
-    "Sleep",-- "SLeep II", "Sleepga", "Sleepga II",
+enfeebling_int = S{
+    "Blind",    -- "Blind II",
+    "Poison",   -- "Poison II",
+    "Sleep",    -- "SLeep II", "Sleepga", "Sleepga II",
     "Break",
     "Bind",
     "Dispel",
-    "Gravity",-- "Gravity II",
-    "Distract",-- "Distract II", "Distract III",
-    "Frazzle",-- "Frazzle II", "Frazzle III"
+    "Gravity",  -- "Gravity II",
+    "Distract", -- "Distract II", "Distract III",
+    "Frazzle",  -- "Frazzle II", "Frazzle III"
 }
 
-enfeebling_mnd = 
-S{
-    "Slow",-- "Slow II",
-    "Paralyze",-- "Paralyze II",
-    "Dia",-- "Dia II", "Dia III", "Diaga",
+enfeebling_mnd = S{
+    "Slow",     -- "Slow II",
+    "Paralyze", -- "Paralyze II",
+    "Dia",      -- "Dia II", "Dia III", "Diaga",
     "Silence",
-    "Addle",
-    "Inundation"
+    "Addle",    -- Addle II
+    "Inundation",
 }
+
 
 -------------------------------------------------------------------------------------------------------------------
 -- Equipment Sets By Slot For Locking
 -------------------------------------------------------------------------------------------------------------------
-equip_lock_earrings = 
-S{
+equip_lock_earrings = S{
     "Reraise Earring",
 }
 
-equip_lock_rings = 
-S{
+equip_lock_rings = S{
     "Emperor Ring",
     "Caliber Ring",
     "Echad Ring",
@@ -104,17 +121,15 @@ S{
     "Dim. Ring (Dem)",
 }
 
-equip_lock_back = 
-S{
+equip_lock_back = S{
     "Nexus Cape",
 }
 
 
 -------------------------------------------------------------------------------------------------------------------
--- Job Sets
+-- Job Sets for Subjobs (For my playstyle)
 -------------------------------------------------------------------------------------------------------------------
-job_type_melee = 
-S{
+job_type_melee = S{
     "WAR",
     "MNK",
     "THF",
@@ -127,11 +142,10 @@ S{
     "RUN",
     "RNG",
     "BST",
-    "COR"
+    "COR",
 }
 
-job_type_mage = 
-S{
+job_type_mage = S{
     "WHM",
     "RDM",
     "BLM",
@@ -139,5 +153,5 @@ S{
     "BLU",
     "SCH",
     "GEO",
-    "SMN"
+    "SMN",,
 }
