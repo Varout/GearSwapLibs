b9addc7b934d13c67a2d44d74f4ca2295d4dfb7f
Jason Auger <jason.auger@gmail.com>
2021-04-09 17:21:35 +1200

    Update/2021 04 09 (#12)
    * SCH whitespace & format changes
    
    * New common function for domain

--- common_functions.lua
+++ common_functions.lua
@@ -1,7 +1,6 @@
 -------------------------------------------------------------------------------------------------------------------
 --  Common Shared Functions
 -------------------------------------------------------------------------------------------------------------------
-
 --  Equipment Related
 all_gear_slots = {
     'main', 'sub',   'range',     'ammo',
@@ -34,4 +33,46 @@ function equipment_unlock_specific(gear_array)
     for iterator, slot in ipairs(gear_array) do
         enable(slot)
     end
+end
+
+
+-------------------------------------------------------------------------------------------------------------------
+--  Common Shared Functions - Domain Invation
+-------------------------------------------------------------------------------------------------------------------
+domain_job = {
+    ['WAR'] = 'heavy', ['PLD'] = 'heavy', ['DRK'] = 'heavy', ['BST'] = 'heavy', ['SAM'] = 'heavy', ['DRG'] = 'heavy',
+    ['MNK'] = 'light', ['THF'] = 'light', ['RNG'] = 'light', ['NIN'] = 'light', ['BLU'] = 'light', ['COR'] = 'light', ['DNC'] = 'light', ['RUN'] = 'light',
+    ['WHM'] = 'mage',  ['BLM'] = 'mage',  ['RDM'] = 'mage',  ['BRD'] = 'mage',  ['SMN'] = 'mage',  ['PUP'] = 'mage',  ['SCH'] = 'mage',  ['GEO'] = 'mage',
+}
+
+domain_set['heavy'] = S{
+    head  = "Hervor Galea",
+    body  = "Hervor Haubert",
+    hands = "Hervor Mouffles",
+    legs  = "Hervor Brayettes",
+    feet  = "Hervor Sollerets",
+}
+
+domain_set['light'] = S{
+    head  = "Heidrek Mask",
+    body  = "Heidrek Harness",
+    hands = "Heidrek Gloves",
+    legs  = "Heidrek Brais",
+    feet  = "Heidrek Boots",
+}
+
+domain_set['mage'] = S{
+    head  = "Angantyr Beret",
+    body  = "Angantyr Robe",
+    hands = "Angantyr Mittens",
+    legs  = "Angantyr Tights",
+    feet  = "Angantyr Boots",
+}
+
+--  Gets the armour set for the job type passed as a parameter
+--  Expects the Char(3) string for the job
+--  Use example: sets.engeged.Domain = get_domain_set(player.main_job)
+function get_domain_set(main_job)
+    local job_type = domain_job[main_job]
+    return domain_set[job_type]
 end
