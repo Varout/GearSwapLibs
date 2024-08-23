b932b826dc92c0776b60ea7c308b7ccd27974111
Jason Auger <jason.auger@gmail.com>
2024-08-23 14:36:00 +0200

    updated terminology on common_objects

--- common_objects.lua
+++ common_objects.lua
@@ -1,20 +1,18 @@
 -------------------------------------------------------------------------------------------------------------------
 --  White Magic
 -------------------------------------------------------------------------------------------------------------------
-
 DivineCaress = {}
 DivineCaress.statusActive = false
 DivineCaress.slot = 'hands'
 DivineCaress.midcast = {
-    hands = "Ebers Mitts +1"    --  Divine Caress +3
+    hands = "Ebers Mitts +3"    --  Divine Caress +4
 }
-function DivineCaress.toggle(status)
+function DivineCaress.toggle(isActive)
+    DivineCaress.statusActive = isActive
     if status then
-        DivineCaress.status = true
         equip(DivineCaress.midcast)
         disable(DivineCaress.slot)
     else
-        DivineCaress.status = false
         enable(DivineCaress.slot)
     end
 end
